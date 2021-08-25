57cf371ce06373798d435dfa359cf12703d0ba94
Rafael Araujo Gomes <rafaelcapgomes@gmail.com>
2021-08-25 20:58:36 -0300

    finished app

--- app/src/main/java/com/cactus/marvelcomics/ui/character/domain/MarvelCharacter.kt
+++
@@ -1,6 +0,0 @@
-package com.cactus.marvelcomics.ui.character.domain
-
-data class MarvelCharacter(
-    val name: String? = null,
-    val thumbnail: String? = null
-)
--- app/src/main/java/com/cactus/marvelcomics/ui/details/presentation/DetailsViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.cactus.marvelcomics.ui.details.presentation
-
-import androidx.lifecycle.ViewModel
-import javax.inject.Inject
-
-class DetailsViewModel @Inject constructor ()  : ViewModel() {
-
-
-}
